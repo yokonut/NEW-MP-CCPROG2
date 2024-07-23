cdb43262e891ed5dcacc58d8006bec86f02443fa
yokonut <koyohan730@gmail.com>
2024-07-24 05:02:46 +0800

    added outputfinal.txt
    it was needed for submission uploading pdf as well

---
+++ ilusrtreS-koY.c
@@ -0,0 +1,38 @@
+#include "function.c"
+
+int main()
+{
+    Entry dictionary[MAX_ENTRIES];
+    int entryCount = 0;
+    int exit = 0;
+    char choice;
+
+    do
+    {
+
+        displayMainMenu();
+        scanf(" %c", &choice);
+        printf("\n");
+
+        switch (choice)
+        {
+        case '1':
+            printf("\e[1;1H\e[2J");
+            manageData(dictionary, &entryCount);
+            break;
+        case '2':
+            printf("\e[1;1H\e[2J");
+            languageTool(dictionary, entryCount);
+            break;
+        case '3':
+            printf("Exiting... Goodbye!\n");
+            exit = 123;
+            break;
+        default:
+            printf("Invalid choice. Please try again.\n");
+            break;
+        }
+    } while (exit == 0);
+
+    return 0;
+}
